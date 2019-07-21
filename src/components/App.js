31ae4a2b359814de4f798b314b27ef9f8c6c831e
Sridharan Soundara Rajan <ss051010@github.cerner.com>
2019-07-21 14:01:21 +0530

    songs application with awesome styles

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SongList from './SongList';
+import SongDetail from './SongDetail';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="ui container grid bordered">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
